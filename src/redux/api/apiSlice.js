3ce3ba8e18d2614dd1d5f29b5d1c2d3bd4c45551
Nikhil kumar <nikhilkumar14082005@gmail.com>
2025-11-28 16:39:20 +0530

    ccc

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -3,14 +3,11 @@ import { BASE_URL } from "../constants.js";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
-  credentials: "include",
-  prepareHeaders: (headers, { endpoint }) => {
-    // Do NOT set Content-Type for image uploads
-    if (endpoint !== "uploadProductImage") {
-      headers.set("Content-Type", "application/json");
-    }
+  credentials: "include",   // ⭐ sends cookies (JWT) with every request
+  prepareHeaders: (headers) => {
+    headers.set("Content-Type", "application/json");
     return headers;
-  },
+  }
 });
 
 export const apiSlice = createApi({
@@ -18,3 +15,7 @@ export const apiSlice = createApi({
   tagTypes: ["Product", "Order", "User", "Category"],
   endpoints: () => ({}),
 });
+
+
+
+
