78c8a89c271399fad745d751e7a8e9ae94c4b607
Nikhil kumar <nikhilkumar14082005@gmail.com>
2025-11-28 18:31:59 +0530

    chang

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -1,22 +1,3 @@
-// import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
-// import { BASE_URL } from "../constants.js";
-
-// const baseQuery = fetchBaseQuery({
-//   baseUrl: BASE_URL,
-//   credentials: "include",   
-//   prepareHeaders: (headers) => {
-//     headers.set("Content-Type", "application/json");
-//     return headers;
-//   }
-// });
-
-// export const apiSlice = createApi({
-//   baseQuery,
-//   tagTypes: ["Product", "Order", "User", "Category"],
-//   endpoints: () => ({}),
-// });
-
-
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../constants.js";
 
@@ -25,10 +6,11 @@ const baseQuery = fetchBaseQuery({
   credentials: "include",
 
   prepareHeaders: (headers, { endpoint }) => {
-    // ❌ Do NOT set content-type for file upload endpoint
+    // ❗ Do NOT set JSON header for file upload
     if (endpoint !== "uploadImage") {
       headers.set("Content-Type", "application/json");
     }
+
     return headers;
   }
 });
@@ -38,5 +20,3 @@ export const apiSlice = createApi({
   tagTypes: ["Product", "Order", "User", "Category"],
   endpoints: () => ({}),
 });
-
-
