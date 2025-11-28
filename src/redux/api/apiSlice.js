3abf281648ac29ea5753313160102c9f9fd8c345
Nikhil kumar <nikhilkumar14082005@gmail.com>
2025-11-28 18:26:19 +0530

    chnages

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -1,11 +1,34 @@
+// import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
+// import { BASE_URL } from "../constants.js";
+
+// const baseQuery = fetchBaseQuery({
+//   baseUrl: BASE_URL,
+//   credentials: "include",   
+//   prepareHeaders: (headers) => {
+//     headers.set("Content-Type", "application/json");
+//     return headers;
+//   }
+// });
+
+// export const apiSlice = createApi({
+//   baseQuery,
+//   tagTypes: ["Product", "Order", "User", "Category"],
+//   endpoints: () => ({}),
+// });
+
+
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../constants.js";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
-  credentials: "include",   // ⭐ sends cookies (JWT) with every request
-  prepareHeaders: (headers) => {
-    headers.set("Content-Type", "application/json");
+  credentials: "include",
+
+  prepareHeaders: (headers, { endpoint }) => {
+    // ❌ Do NOT set content-type for file upload endpoint
+    if (endpoint !== "uploadImage") {
+      headers.set("Content-Type", "application/json");
+    }
     return headers;
   }
 });
@@ -15,7 +38,5 @@ export const apiSlice = createApi({
   tagTypes: ["Product", "Order", "User", "Category"],
   endpoints: () => ({}),
 });
-
-
 
 
